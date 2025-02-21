5e0b6ae7cadfa2fe153124a3712e3854ba9ba4ee
Schorschi <gmuglach@tsn.at>
2025-02-21 09:19:47 +0100

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -5,19 +5,21 @@ import socket
 HOST = "192.168.0.100"  # Standard loopback interface address (localhost)
 PORT = 4444  # Port to listen on (non-privileged ports are > 1023)
 
-U=2#V
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data=conn.recv(1024)
-            if not data:
-                break
-            data=str(data,"utf-8")
-            #data = json.loads(data)
-            print(data)
-         #   elif data=="bereit":
-          #      conn.sendall(bin(U))
+
+while True:
+    U=2#V
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data=conn.recv(1024)
+                if not data:
+                    break
+                data=str(data,"utf-8")
+                #data = json.loads(data)
+                print(data)
+             #   elif data=="bereit":
+              #      conn.sendall(bin(U))
