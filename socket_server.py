3efb7b3d1516accf35b6bdf11421aa5377526a1b
Schorschi <gmuglach@tsn.at>
2025-02-21 10:09:21 +0100

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -27,9 +27,8 @@ HOST = "192.168.0.100"  # Standard loopback interface address (localhost)
 PORT = 4444  # Port to listen on (non-privileged ports are > 1023)
 
 
-
+#empfangenhhhhhhhhh
 while True:
-    U=2#V
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
