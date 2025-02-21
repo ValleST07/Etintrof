3bcea70e36cb4d803f53c47cb5313f38668be61a
Fellex1 <graneggerfelix@gmail.com>
2025-02-21 09:56:44 +0100

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -1,6 +1,24 @@
 import socket
 import json
 
+def antwort():
+    U=2#V
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('192.168.0.101',4444))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data=conn.recv(1024)
+                if not data:
+                    break
+                data=str(data,"utf-8")
+                print(data)
+
+
+
+
 while True:
     use = input ("What to you want to send->")
     if use:
@@ -9,7 +27,6 @@ while True:
 
         s=socket.socket()
         s.connect(addr)
-        x="10909.90809809.1980, Fertig"
 
         #data = json.dumps(x).encode() 
         data= bytes(f"{use}","utf-8")
@@ -18,3 +35,6 @@ while True:
 
         #print("Received "+str(data,"utf-8"))
         s.close()
+        antwort()
+        
+        
