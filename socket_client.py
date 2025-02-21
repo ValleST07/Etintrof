69c29ad70d324f4c8cf71a076c97b8fd1cb46be5
Fellex1 <graneggerfelix@gmail.com>
2025-02-21 09:15:58 +0100

    Update socket_client.py

--- socket_client.py
+++ socket_client.py
@@ -1,18 +1,20 @@
 import socket
 import json
 
+while True:
+    use = input ("What to you want to send->")
+    if use:
+        #Use IP address of Socket server and non-privileged port >1023
+        addr=('192.168.0.100',4444)
 
-#Use IP address of Socket server and non-privileged port >1023
-addr=('192.168.0.100',4444)
+        s=socket.socket()
+        s.connect(addr)
+        x="10909.90809809.1980, Fertig"
 
-s=socket.socket()
-s.connect(addr)
-x="Hallo"
+        #data = json.dumps(x).encode() 
+        data= bytes(f"{use}","utf-8")
+        s.sendall(data)
+        #data = s.recv(1024)
 
-#data = json.dumps(x).encode() 
-data= bytes(f"{x}","utf-8")
-s.sendall(data)
-#data = s.recv(1024)
-
-#print("Received "+str(data,"utf-8"))
-s.close()
+        #print("Received "+str(data,"utf-8"))
+        s.close()
