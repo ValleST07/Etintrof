5faef617647eb7bb4326a72bd24a6b54a5df4e6c
Paul File <pfile6533@gmail.com>
2025-04-11 08:43:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import subprocess
-import sys
-import os
+import runpy
 import pygame
 from pygame import Rect
-import signal
 
 # Farben
 WHITE = (255, 255, 255)
@@ -23,44 +20,6 @@ clock = pygame.time.Clock()
 # Buttons
 server_button = Rect(100, 100, 200, 50)
 ui_button = Rect(100, 180, 200, 50)
-
-def cleanup():
-    """Zurück zu main und Pygame schließen"""
-    try:
-        subprocess.run(["git", "checkout", "main"], stderr=subprocess.DEVNULL)
-    except:
-        pass
-    pygame.quit()
-    sys.exit(0)
-
-def signal_handler(sig, frame):
-    cleanup()
-
-# Strg+C Signalhandler
-signal.signal(signal.SIGINT, signal_handler)
-
-
-def run_script(branch, script_name):
-    project_dir = os.path.dirname(os.path.abspath(__file__))
-
-    try:
-        subprocess.run(["git", "rev-parse", "--git-dir"], check=True, cwd=project_dir)
-        subprocess.run(["git", "checkout", branch], check=True, cwd=project_dir)
-        print(f"[Info] Switched to branch '{branch}'")
-
-        pygame.display.quit()
-
-        process = subprocess.Popen([sys.executable, script_name], cwd=project_dir)
-
-        process.wait()
-        return True
-
-    except subprocess.CalledProcessError as e:
-        return False
-
-    finally:
-        subprocess.run(["git", "checkout", "main"], check=True, cwd=project_dir)
-
 
 def draw_menu():
     screen.fill(WHITE)
@@ -82,27 +41,22 @@ def draw_menu():
 
     pygame.display.flip()
 
-try:
-    running = True
-    while running:
-        draw_menu()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+running = True
+while running:
+    draw_menu()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = pygame.mouse.get_pos()
+
+            if server_button.collidepoint(mouse_pos):
+                runpy.run_path("Server.py")
                 running = False
-
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-
-                if server_button.collidepoint(mouse_pos):
-                    if run_script("server", "server.py"):
-                        running = False
-
-                elif ui_button.collidepoint(mouse_pos):
-                    if run_script("UI", "MapUITest.py"):
-                        running = False
-
-        clock.tick(60)
-finally:
-    cleanup()
-
+            elif ui_button.collidepoint(mouse_pos):
+                runpy.run_path("MapUITest.py")
+                running = False
+    clock.tick(60)
